48bc9fa8cdf34825e86f8109a04c8ad928120a7f
Source Academy User <No public email provided>
2023-09-21 09:29:18 +0800

    Changes made from Source Academy

--- reflection6.js
+++ reflection6.js
@@ -17,13 +17,31 @@ const xs = list(6, 3, 8, 5, 1, 9, 6, 4, 2 ,7);
 
 
 // qn 1a
-insertion_sort_cmp(xs, (x,y) => x < y);
+// insertion_sort_cmp(xs, (x,y) => x <= y);
 // qn 1b
-insertion_sort_cmp(xs, (x,y) => x > y);
+// insertion_sort_cmp(xs, (x,y) => x >= y);
 // qn 1c
-insertion_sort_cmp(xs, (x,y) => );
+// insertion_sort_cmp(xs, (x,y) => false);
 // qn 1d
-// insertion_sort_cmp(xs, (x,y) => x % 2 === 0);
-insertion_sort_cmp(xs, (x,y) => x % 2 === 0 ? x < y : x > y);
+function is_even(x){
+    return x % 2 === 0;
+}
+function dHelper(x, y){
+    if (is_even(x) && is_even(y)){
+        return x <= y;
+    } else if (!is_even(x) && !is_even(y)){
+        return x > y;
+    } else if (is_even(x) && !is_even(y)){
+        return true;
+    } else {
+      return false;  
+    }
+}
+insertion_sort_cmp(xs, dHelper);
 
+// insertion_sort_cmp(xs, (x,y) => x % 2 === 0 ? x < y : x > y);
+// qn 2a
+// O(n), theta(n), omega(1)
+// qn 2b
+// O(n^2), theta(n), omega(1)
  
