e0c469bf6b286ebde21a8c84eb0964c38c8ba6c8
Source Academy User <No public email provided>
2023-10-26 17:09:34 +0800

    Changes made from Source Academy

--- reflection9.js
+++ reflection9.js
@@ -1,18 +1,65 @@
-function fib(n){
-    const f = [0,1];
-    // not < n, as that stops just before calculating fib n
-    for (let i = 2; i <= n; i = i+1){
-        f[i] =  f[i-1] + f[i-2];
+function memo_fun(fun) {
+    let already_run = false;
+    let result = undefined;
+
+    function mfun() {
+        if (!already_run) {
+            result = fun();
+            already_run = true;
+            return result;
+        } else {
+            return result;
+        }
     }
-    return f[n];
+    return mfun;
 }
 
-function fib_1(n){
-    let a = 0;
-    let b = 1;
-    for (let i = 2; i <= n; i = i + 1){
-        b = b + a;
-        a = b - a;
-    }
-    return b;
+
+function stream_map_optimized(f, s) {
+  return is_null(s)
+    ? null
+    : pair(
+        f(head(s)),
+        memo_fun(() => stream_map_optimized(f, stream_tail(s)))
+      );
+}
+
+// displays 0
+const x = stream_map_optimized(display, enum_stream(0, 10));
+// displays 1,2,3
+stream_ref(x, 3);
+// displays 4,5 as 1,2,3 are already memoized
+stream_ref(x, 5);
+
+// function zip_list_of_streams(xs) {
+//   const len = length(xs);
+//   function zipHelper(xs, counter) {
+//     for (let i = 0; i < len; i = i + 1) {
+//       if (i === len - 1) {
+//         pair(list_ref(xs, i), () => zipHelper(tail(xs), 0));
+//       }
+//       pair(head(list_ref(xs, i)), () => head(list_ref(xs, i+1));
+//     }
+//   }
+// }
+
+function zip_list_of_streams(ss){
+    return pair(head(head(ss)),
+        () => zip_list_of_streams(
+                append(tail(ss),
+                    list(stream_tail(head(ss))))));
+}
+
+function partial_sums(s) {
+    function add_streams(s1, s2) {
+        return is_null(s1)
+            ? s2
+            : is_null(s2)
+            ? s1
+            : pair(head(s1) + head(s2),
+                   () => add_streams(stream_tail(s1), 
+                                     stream_tail(s2)));
+        }
+    return pair(head(s), () => add_streams(stream_tail(s),
+                            partial_sums(s)));
 }
